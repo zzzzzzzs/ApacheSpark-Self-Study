0588a5d9289eda836b1464f62de16df18ff014c8
zzzzzzzs <1443539042@qq.com>
2021-03-16 23:05:29 +0800

    Create Spark03_Acc3.scala

---
+++ SparkCore/src/main/java/spark/core/acc/Spark03_Acc3.scala
@@ -0,0 +1,40 @@
+package spark.core.acc
+
+import org.apache.spark.util.AccumulatorV2
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
+
+object Spark03_Acc3 {
+
+    def main(args: Array[String]): Unit = {
+        // TODO Spark - 累加器
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+        val sc = new SparkContext(sparkConf)
+
+        val rdd = sc.makeRDD(
+            List(1, 2, 3, 4)
+        )
+
+        // 创建累加器
+        val sumAcc = sc.longAccumulator("sum")
+
+        val mapRDD = rdd.map(
+            num => {
+                // 使用累加器
+                sumAcc.add(num)
+                num
+            }
+        )
+
+        // 获取累加器的值
+        // 少加:转换算子中调用累加器，如果没有行动算子的话，那么不会执行
+        // 多加:转换算子中调用累加器，如果没有行动算子的话，那么不会执行
+        // 一般情况下，累加器会放在行动算子中操作
+        mapRDD.collect()
+        mapRDD.collect()
+        println(sumAcc.value)
+
+        sc.stop()
+    }
+}
